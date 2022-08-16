c7e7a11f40c5ace127b8656bfe88eae47db00345
Juemin Xu <jueminxu777@gmail.com>
2022-08-16 01:18:57 +0100

    remove welcome

--- digitalClock.py
+++ digitalClock.py
@@ -3,7 +3,7 @@ window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
 window.configure(bg="blue")
-lable = Label(window, text ="Welcome!", font = ("Times New Roman", 78, "bold"), bg="blue")
+lable = Label(window, text ="W", font = ("Times New Roman", 78, "bold"), bg="blue")
 label.pack(pady=100)
 window.mainloop()
 
